68500578dc4df2c7f00c3fad4191190c8881a3fd
David042 <Davidsarka242@gmail.com>
2021-02-28 08:33:29 -0800

    added the rating section

--- app/Views/layer.php
+++ app/Views/layer.php
@@ -6,25 +6,63 @@
 
 <div class="container">
 	<h2>Active Games</h2>
+	<form>
+		<p class="search-icon">
+			<input type="search" name="search" placeholder="search">
+			<i class="fa fa-search"></i>
+		</p>
+	</form>
+	<div class="first-row">
+		<div class="pport-holder rating-section">
+			<img src="https://portal.uniabuja.edu.ng/pictures/16291165.jpg" alt="Nancee Image">
+		</div>
+		<div class="rating-catption">
+			<h2>Assassins Creed Valhalla</h2>
+			<p>PS5 Version</p>
+			<input type="range" name="range">
+		</div>
+		
+	</div>
+	<div class="first-row second-row">
+		<div class="pport-holder rating-section">
+			<img src="https://portal.uniabuja.edu.ng/pictures/16291165.jpg" alt="Nancee Image">
+		</div>
+		<div class="rating-catption">
+			<h2>SpiderMan - Miles Morales</h2>
+			<p>PS5 Version</p>
+			<input type="range" name="range">
+		</div>
+		
+	</div>
+	<div class="first-row third-row">
+		<div class="pport-holder rating-section">
+			<img src="https://portal.uniabuja.edu.ng/pictures/16291165.jpg" alt="Nancee Image">
+		</div>
+		<div class="rating-catption">
+			<h2>Sackboy - A Big Adventure</h2>
+			<p>PS5 Version</p>
+			<input type="range" name="range">
+		</div>
+		
+	</div>
 </div>
 <div class="container option-cont">
-	<h2>Options</h2>
-
-	<div class="pport-holder">
-		<img src="nance.jpg" alt="Nancee Image">
+	<div class="pport-holder small-pport">
+		<img src="https://portal.uniabuja.edu.ng/pictures/16291165.jpg" alt="Nancee Image">
 	</div>
 	<div class="status-holder">
 	<h3>Nancee Aka</h3>
 	<p>Pro Member</p>
 	</div>	
 	<div class="status-holder items-holder">
-	<p><i class="fa fa-square"></i> Nancee Aka</p>
-	<p><i class="fa fa-square"></i> Nancee Aka</p>
-	<p><i class="fa fa-square"></i> Nancee Aka</p>
-	<p><i class="fa fa-square"></i> Nancee Aka</p>
+	<p><i class="fa fa-tv"></i>Streams</p>
+	<p><i class="fa fa-binoculars"></i>Games</p>
+	<p><i class="fa fa-gamepad"></i> Upcomming</p>
+	<p><i class="fa fa-book"></i> Library</p>
 	</div>
 	<div class="status-holder item-holder join-holder">
-	<p><i class="fa fa-square"></i> Nancee Aka</p>
-	<p><i class="fa fa-square"></i> Nancee Aka</p>
+	<p>Join Pro</p>
+	<p>For Free Games</p>
+	<i class="fa fa-gamepad 3x"></i>
 	</div>	
 </div>
